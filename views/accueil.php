69f7860d60abf2d94008084e917902dc3720c9ad
Joris Seyadou <Joris Seyadou>
2017-04-27 23:47:44 +0200

    Rajout bloc bienvenue

--- views/accueil.php
+++ views/accueil.php
@@ -8,26 +8,26 @@
     <!-- Slides -->
     <div class="carousel-inner">
         <!-- Page 1 -->
-        <div class="item active">  
+        <div class="item active">
             <div class="carousel-page">
                 <img src="<?php echo $path['img']; ?>slider1.jpg" class="img-responsive" style="margin:0px auto;" />
-            </div> 
+            </div>
             <div class="carousel-caption">Trouvez votre bonheur dans le sport</div>
-        </div>   
+        </div>
         <!-- Page 2 -->
-        <div class="item"> 
+        <div class="item">
             <div class="carousel-page">
                 <img src="<?php echo $path['img']; ?>slider2.jpg" class="img-responsive" style="margin:0px auto;"  />
-            </div> 
+            </div>
         <div class="carousel-caption">Dépassez vos limites</div>
-        </div>  
+        </div>
         <!-- Page 3 -->
-        <div class="item">  
+        <div class="item">
             <div class="carousel-page">
                 <img src="<?php echo $path['img']; ?>slider3.jpg" class="img-responsive img-rounded" style="margin:0px auto;"  />
-            </div>  
+            </div>
             <div class="carousel-caption">Trouvez votre partenaire de sport</div>
-        </div>     
+        </div>
     </div>
     <!-- Contrôles -->
     <a class="left carousel-control" href="#bjn-carousel" data-slide="prev">
@@ -37,3 +37,10 @@
         <span class="glyphicon glyphicon-chevron-right"></span>
     </a>
 </div>
+<section id="bienvenue" class="container-fluid">
+    <div class="heading">
+        <h1>Bienvenue sur SOS Partner!</h1>
+        <h3>Le site où vous trouverez votre partenaire</h3>
+        <a href="inscription.php" class="bouton1">Inscrivez-vous</a>
+    </div>
+</section>
