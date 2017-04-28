c4cb2e12a96fe283d281d1b2a34b60d0cdbb97a8
jeancy <gencyblondin@gmail.com>
2017-04-28 12:30:21 +0200

    Rajout de la connexion dans la navbar

--- public/elt/header.php
+++ public/elt/header.php
@@ -14,6 +14,42 @@
                     <li><a href="<?php echo $link['accueil']; ?>">Accueil</a></li>
                     <li><a href="<?php echo $link['annonces']; ?>">Annonces</a></li>
                  </ul>
+                 <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span class="caret"></span></a>
+                            <ul id="login-dp" class="dropdown-menu">
+                                <li>
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <form class="form" role="form" method="post" action="#" accept-charset="UTF-8" id="login-nav">
+                                                <div class="form-group">
+                                                    <label class="sr-only" for="exampleInputEmail2">Email</label>
+                                                    <input type="email" class="form-control" id="exampleInputEmail2" name="email" placeholder="Email" required>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label class="sr-only" for="exampleInputPassword2">Mot de passe</label>
+                                                    <input type="password" class="form-control" id="exampleInputPassword2" name="mdp" placeholder="Mot de passe" required>
+                                                    <div class="help-block text-right"><a href="#">Mot de passe oublié ?</a></div>
+                                                </div>
+                                                <div class="form-group">
+                                                    <button type="submit" name="valider" class="btn btn-primary btn-block">Sign in</button>
+                                                </div>
+                                                <div class="checkbox">
+                                                    <label>
+                                                    <input type="checkbox" name="souvenir"> Se souvenir de moi
+                                                    </label>
+                                                </div> 
+                                            </form>
+                                        </div>
+                                        <div class="bottom text-center">
+                                           Pas encore inscrit ? <a href="#"><b>Inscription</b></a>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>   
+                    </li>
+                </ul>
+
             </div>
         </div>
     </nav>
