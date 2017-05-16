795c6dc2bdf0d61b3caaeccb6c0de144917edd51
nhut94 <nhut@hotmail.fr>
2017-05-16 20:13:21 +0200

    Nouvelle page d'accueil

--- views/accueil.php
+++ views/accueil.php
@@ -1,40 +1,38 @@
-<div id="bjn-carousel" class="carousel slide" data-ride="carousel">
-    <!-- Bulles -->
-    <ol class="carousel-indicators">
-        <li data-target="#bjn-carousel" data-slide-to="0" class="active"></li>
-        <li data-target="#bjn-carousel" data-slide-to="1"></li>
-        <li data-target="#bjn-carousel" data-slide-to="2"></li>
-    </ol>
-    <!-- Slides -->
-    <div class="carousel-inner">
-        <!-- Page 1 -->
-        <div class="item active">
-            <img src="<?php echo $path['img']; ?>slider1.jpg" class="img-responsive" style="width: 100%;"/>
-            <div class="carousel-caption">Trouvez votre bonheur dans le sport</div>
-        </div>
-        <!-- Page 2 -->
-        <div class="item">
-            <img src="<?php echo $path['img']; ?>slider2.jpg"  class="img-responsive" style="width: 100%;"/>
-             <div class="carousel-caption">Dépassez vos limites</div>
-        </div>
-        <!-- Page 3 -->
-        <div class="item">
-            <img src="<?php echo $path['img']; ?>slider3.jpg" style="width: 100%;"/>
-            <div class="carousel-caption">Trouvez votre partenaire de sport</div>
+<section id="home">
+    <div class="container-fluid">
+        <div class="container"> 
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="<?php echo $path['img'].'sectionaccueil1.jpg'; ?>" alt="">
+                </div>
+                <div class="col-md-8">
+                    <h2>SOS Partner</h2>
+                    <p>Vous êtes sportif régulier, du dimanche ou expérimenté? Alors bienvenue dans SOS Partner, le site de rencontre sportif en France. Découvrez des membres qui pratiquent le même sport     que vous: des boxeurs, des athlètes, des lutteurs, des danseuses étoiles, des passionnés de randos, des skieurs, des nageurs... Envoyez leur des balles pour attirer leur attention et  discutez avec eux. Il va y avoir du sport ... et plus si affinités.</p>
+                </div>
+            </div>
         </div>
     </div>
-    <!-- Contrôles -->
-    <a class="left carousel-control" href="#bjn-carousel" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left"></span>
-    </a>
-    <a class="right carousel-control" href="#bjn-carousel" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right"></span>
-    </a>
-</div>
-<section id="bienvenue" class="container-fluid">
-    <div class="heading">
-        <h1>Bienvenue sur SOS Partner!</h1>
-        <h3>Le site où vous trouverez votre partenaire</h3>
-        <a href="inscription.php" class="bouton1">Inscrivez-vous</a>
+    <div class="container-fluid bluebackground">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="center">Comment fonctionne SOS Partner?</h2>
+                </div>
+            </div>
+            <div class="row center" style="margin-top: 20px;">
+                <div class="col-md-4">
+                    <h2>Configurez</h2>
+                    <p>Renseignez les sports pour lesquels vous souhaitez apparaître dans la recherche parmis nos 13 sports disponibles.</p>
+                </div>
+                <div class="col-md-4">
+                    <h2>Recherchez</h2>
+                    <p>Choisissez le sport, la distance autour de vous ou de la ville sélectionnée, le sexe et le niveau du partenaire que vous recherchez.</p>
+                </div>
+                <div class="col-md-4">
+                    <h2>Discutez</h2>
+                    <p>Grâce à une messagerie instantannée, échangez avec vos nouveaux contacts sportifs pour organiser votre rencontre sportive.</p>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
--- views/inscription.php
+++
@@ -1,3 +0,0 @@
-<div class="thumbnail">
-  <img src="<?php echo $path['img']; ?>background-inscription.jpg" alt="background" class="img-rounded">
-</div>
