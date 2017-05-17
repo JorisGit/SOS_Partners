44a767e4a7a322302edda3512042c06d179ca334
nhut94 <nhut@hotmail.fr>
2017-05-17 23:23:53 +0200

    -1 ligne

--- public/elt/header.php
+++ public/elt/header.php
@@ -17,45 +17,44 @@
                  <ul class="nav navbar-nav navbar-right">
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span class="caret"></span></a>
-                            <ul id="login-dp" class="dropdown-menu">
-                                <li>
-                                    <div class="row">
-                                        <div class="col-md-12">
-                                            <form class="form" role="form" method="post" action="#" accept-charset="UTF-8" id="login-nav">
-                                                <div class="form-group">
-                                                    <label class="sr-only" for="exampleInputEmail2">Email</label>
-                                                    <input type="email" class="form-control" id="exampleInputEmail2" name="email" placeholder="Email" required>
-                                                </div>
-                                                <div class="form-group">
-                                                    <label class="sr-only" for="exampleInputPassword2">Mot de passe</label>
-                                                    <input type="password" class="form-control" id="exampleInputPassword2" name="mdp" placeholder="Mot de passe" required>
-                                                    <div class="help-block text-right"><a href="#">Mot de passe oublié ?</a></div>
-                                                </div>
-                                                <div class="form-group">
-                                                    <button type="submit" name="valider" class="btn btn-primary btn-block">Connexion</button>
-                                                </div>
-                                                <div class="checkbox">
-                                                    <label>
-                                                    <input type="checkbox" name="souvenir"> Se souvenir de moi
-                                                    </label>
-                                                </div> 
-                                            </form>
-                                        </div>
-                                        <?php 
-                                            if(substr_compare($_SERVER['REQUEST_URI'], $link['inscription'], strlen($link['inscription'])*-1, strlen($link['inscription']))) {
-                                        ?>
-                                        <div class="bottom text-center">
-                                           Pas encore inscrit ? <a href="<?php echo $page['inscription']; ?>"><b>Inscription</b></a>
-                                        </div>
-                                        <?php
-                                            }
-                                        ?>
+                        <ul id="login-dp" class="dropdown-menu">
+                            <li>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <form class="form" role="form" method="post" action="#" accept-charset="UTF-8" id="login-nav">
+                                            <div class="form-group">
+                                                <label class="sr-only" for="exampleInputEmail2">Email</label>
+                                                <input type="email" class="form-control" id="exampleInputEmail2" name="email" placeholder="Email" required>
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="sr-only" for="exampleInputPassword2">Mot de passe</label>
+                                                <input type="password" class="form-control" id="exampleInputPassword2" name="mdp" placeholder="Mot de passe" required>
+                                                <div class="help-block text-right"><a href="#">Mot de passe oublié ?</a></div>
+                                            </div>
+                                            <div class="form-group">
+                                                <button type="submit" name="valider" class="btn btn-primary btn-block">Connexion</button>
+                                            </div>
+                                            <div class="checkbox">
+                                                <label>
+                                                <input type="checkbox" name="souvenir"> Se souvenir de moi
+                                                </label>
+                                            </div> 
+                                        </form>
                                     </div>
-                                </li>
-                            </ul>   
+                                    <?php 
+                                        if(substr_compare($_SERVER['REQUEST_URI'], $link['inscription'], strlen($link['inscription'])*-1, strlen($link['inscription']))) {
+                                    ?>
+                                    <div class="bottom text-center">
+                                        Pas encore inscrit ? <a href="<?php echo $page['inscription']; ?>"><b>Inscription</b></a>
+                                    </div>
+                                    <?php
+                                        }
+                                    ?>
+                                </div>
+                            </li>
+                        </ul>   
                     </li>
                 </ul>
-
             </div>
         </div>
     </nav>
