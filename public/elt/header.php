1ecac222b934b27867532e301e8eb882aba6e5f2
nhut94 <nhut@hotmail.fr>
2017-05-17 23:58:39 +0200

    Connexion blondin

--- public/elt/header.php
+++ public/elt/header.php
@@ -14,34 +14,33 @@
                     <li><a href="<?php echo $link['accueil']; ?>">Accueil</a></li>
                     <li><a href="<?php echo $link['annonces']; ?>">Annonces</a></li>
                  </ul>
+                 <?php if(isset($_SESSION['pseudo'])){?>
+                    <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b><?php echo $_SESSION['pseudo'] ?></b> <span class="caret"></span></a>
+                            <ul id="login-dp" class="dropdown-menu">
+                                <li>
+                                    <div class="row">
+                                        <div class="avatarHeader">
+                                            <img src="<?php echo $path['img'].'avatars/'.$_SESSION['avatar'];?>" class="avatar img-circle img-thumbnail" alt="avatar" style="width:50%;">
+                                        </div>
+                                        <div class="bottom text-center">
+                                            <span class="glyphicon glyphicon-user" aria-hidden="true"> </span>
+                                            <a href=""><b>Profil</b></a>
+                                        </div>
+                                        <div class="bottom text-center">
+                                            <span class="glyphicon glyphicon-off" aria-hidden="true"> </span>
+                                            <a href="<?php echo $link['deconnexion']; ?>"><b>Se deconnecter</b></a>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>   
+                    </li>
+                </ul>
+                 <?php }else{?>
                  <ul class="nav navbar-nav navbar-right">
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span class="caret"></span></a>
-<<<<<<< HEAD
-                        <ul id="login-dp" class="dropdown-menu">
-                            <li>
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <form class="form" role="form" method="post" action="#" accept-charset="UTF-8" id="login-nav">
-                                            <div class="form-group">
-                                                <label class="sr-only" for="exampleInputEmail2">Email</label>
-                                                <input type="email" class="form-control" id="exampleInputEmail2" name="email" placeholder="Email" required>
-                                            </div>
-                                            <div class="form-group">
-                                                <label class="sr-only" for="exampleInputPassword2">Mot de passe</label>
-                                                <input type="password" class="form-control" id="exampleInputPassword2" name="mdp" placeholder="Mot de passe" required>
-                                                <div class="help-block text-right"><a href="#">Mot de passe oublié ?</a></div>
-                                            </div>
-                                            <div class="form-group">
-                                                <button type="submit" name="valider" class="btn btn-primary btn-block">Connexion</button>
-                                            </div>
-                                            <div class="checkbox">
-                                                <label>
-                                                <input type="checkbox" name="souvenir"> Se souvenir de moi
-                                                </label>
-                                            </div> 
-                                        </form>
-=======
                             <ul id="login-dp" class="dropdown-menu">
                                 <li>
                                     <div class="row">
@@ -75,22 +74,12 @@
                                         <?php
                                             }
                                         ?>
->>>>>>> d950c5c24b06f2c9f2d14f29ed591eb2af73f5eb
                                     </div>
-                                    <?php 
-                                        if(substr_compare($_SERVER['REQUEST_URI'], $link['inscription'], strlen($link['inscription'])*-1, strlen($link['inscription']))) {
-                                    ?>
-                                    <div class="bottom text-center">
-                                        Pas encore inscrit ? <a href="<?php echo $page['inscription']; ?>"><b>Inscription</b></a>
-                                    </div>
-                                    <?php
-                                        }
-                                    ?>
-                                </div>
-                            </li>
-                        </ul>   
+                                </li>
+                            </ul>   
                     </li>
                 </ul>
+                 <?php }?>
             </div>
         </div>
     </nav>
