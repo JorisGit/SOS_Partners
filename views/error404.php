2ae872bc1797566274011d2f66ef39462454364a
JorisGIT <joris@behind-shop.fr>
2017-05-18 13:45:56 +0200

    Ajout de la page d'erreur 404

---
+++ views/error404.php
@@ -0,0 +1,16 @@
+<div class="erreur-404">
+    <div class="erreur-code m-b-10 m-t-20">404 <i class="fa fa-warning"></i></div>
+    <h3 class="font-bold">Cette page n'existe pas..</h3>
+
+    <div class="erreur-404-desc">
+        <p>Désolé, la page que vous essayer d'accéder est inexistante ou inacessible.</p>
+        <p>Essayer de rafraîchir la page où si le problème persiste de nous contacter par email.</p>
+        <div>
+            <a class="login-detail-panel-button btn" href="http://www.sospartner.behind-shop.fr/">
+                <i class="fa fa-arrow-left"></i>
+                Retourner sur la page d'accueil
+            </a>
+        </div>
+    </div>
+
+</div>
