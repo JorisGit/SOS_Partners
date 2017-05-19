52c2e91ce16ae531fe954bb479fb7724e213a60a
nhut94 <nhut@hotmail.fr>
2017-05-19 20:17:07 +0200

    Class Profil

---
+++ models/Profil.php
@@ -0,0 +1,131 @@
+<?php
+
+class Profil {
+
+    private $_pseudo;
+    private $_mdp;
+    private $_email;
+    private $_newsletter;
+    private $_prenom;
+    private $_nom;
+    private $_sexe;
+    private $_dateNaissance;
+    private $_departement;
+    private $_ville;
+
+    public function __construct($pseudo, $mdp, $email, $newsletter, $prenom, $nom, $sexe, $dateNaissance, $departement, $ville) {
+        $this->setPseudo($pseudo);
+        $this->setMdp($mdp);
+        $this->setEmail($email);
+        $this->setNewsletter($newsletter);
+        $this->setPrenom($prenom);
+        $this->setNom($nom);
+        $this->setSexe($sexe);
+        $this->setDateNaissance($dateNaissance);
+        $this->setDepartement($departement);
+        $this->setVille($ville);
+    }
+
+    public function setPseudo($pseudo) {
+        if(strlen($pseudo) > 12) {
+            trigger_error('Le pseudo ne doit pas dépasser 12 caractères', E_USER_WARNING);
+            return;
+        }
+
+        $this->_pseudo = $pseudo;
+    }
+
+    public function setMdp($mdp) {
+        $this->_mdp = $mdp;
+    }
+
+    public function setEmail($email) {
+        $this->_email = $email;
+    }
+
+    public function setNewsletter($newsletter) {
+        $this->_newsletter = $newsletter;
+    }
+
+    public function setPrenom($prenom) {
+        $this->_prenom = $prenom;
+    }
+
+    public function setNom($nom) {
+        $this->_nom = $nom;
+    }
+
+    public function setSexe($sexe) {
+        $this->_sexe = $sexe;
+    }
+
+    public function setDateNaissance($dateNaissance) {
+        $this->_dateNaissance = $dateNaissance;
+    }
+
+    public function setDepartement($departement) {
+        $this->_departement = $departement;
+    }
+
+    public function setVille($ville) {
+        $this->_ville = $ville;
+    }
+
+    public function getPseudo() {
+        return $this->_pseudo;
+    }
+
+    public function getMdp() {
+        return $this->_mdp;
+    }
+
+    public function getEmail() {
+        return $this->_email;
+    }
+
+    public function getPrenom() {
+        return $this->_prenom;
+    }
+
+    public function getNom() {
+        return $this->_nom;
+    }
+
+    public function getSexe() {
+        return $this->_sexe;
+    }
+
+    public function getDateNaissance() {
+        return $this->_dateNaissance;
+    }
+
+    public function getDepartement() {
+        return $this->_departement;
+    }
+
+    public function getVille() {
+        return $this->_ville;
+    }
+
+    public function insertProfil($pseudo) {
+        $bdd = getBdd();
+        $req = $bdd->prepare('
+        INSERT INTO profil(pseudo)
+        VALUES(:pseudo)
+        ');
+        $req->execute(array(
+            'pseudo' => $this->_pseudo/*,
+            'mdp' => $this->_mdp,
+            'email' => $this->_email,
+            'newsletter' => $this->_newsletter,
+            'prenom' => $this->_prenom,
+            'nom' => $this->_nom,
+            'sexe' => $this->_sexe,
+            'dateNaissance' => $this->_dateNaissance,
+            'departement' => $this->_departement,
+            'ville' => $this->_ville*/
+        ));
+    }
+}
+
+?>
