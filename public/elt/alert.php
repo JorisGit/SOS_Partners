879d45949f79e9cf39e4df56d954c5570913c418
nhut94 <nhut@hotmail.fr>
2017-05-21 21:31:08 +0200

    glyphicon & alert

--- public/elt/alert.php
+++ public/elt/alert.php
@@ -2,7 +2,12 @@
     <div class="row">
         <div class="col-md-3"></div>
         <div class="col-md-6">
-            <div class="alert alert-danger center" role="alert"><span class="glyphicon glyphicon-alert"></span> <?php echo $alert; ?></div>
+            <div class="alert alert-danger center .alert-dismissible" role="alert">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <span class="glyphicon glyphicon-alert position-icon"></span> <?php echo $alert; ?>
+            </div>
         </div>
     </div>
 <?php } ?>
