56be5f53c5bc3bdea842d5c4d348a87b395929de
nhut94 <nhut@hotmail.fr>
2017-05-21 21:12:12 +0200

    Messages d'alertes

---
+++ public/elt/alert.php
@@ -0,0 +1,8 @@
+<?php if(isset($alert)) { ?>
+    <div class="row">
+        <div class="col-md-3"></div>
+        <div class="col-md-6">
+            <div class="alert alert-danger center" role="alert"><span class="glyphicon glyphicon-alert"></span> <?php echo $alert; ?></div>
+        </div>
+    </div>
+<?php } ?>
