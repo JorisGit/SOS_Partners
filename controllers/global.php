8d35a455450db6da2976563a2349ca27d679d0eb
nhut94 <nhut@hotmail.fr>
2017-05-30 20:53:42 +0200

    Affichage du profil avec $myProfil

--- controllers/global.php
+++ controllers/global.php
@@ -7,10 +7,18 @@
 
     if(isset($_SESSION['pseudo'])) {
         
+        $connexion = new ProfilManager(getDb());
+        $myProfil = $connexion->get($_SESSION['pseudo']);
+        
+        $pseudo = $_SESSION['pseudo'];
+        $pseudoClass = compact('pseudo');
+
+        $profil = new Profil($pseudoClass);
+
     }
 
     if(isset($_POST['login'])){
-
+        
         $identifiant = htmlspecialchars($_POST['identifiant']);
         $mdp = htmlspecialchars(hash(hash_algos()[7], $_POST['mdpLog']));
 
@@ -25,7 +33,7 @@
         $log = $connexion->loginCompte($identifiant, $mdp, $souvenir);
 
         if($log == false) {
-            $alert = "Erreur de pseudo/email ou de mot de passe.";
+            $alert = "Pseudo/email ou mot de passe incorrect.";
         }
     }
     
