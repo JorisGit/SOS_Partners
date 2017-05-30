f9bf8d43ed96392c11c8b85e60a908a4289deb26
nhut94 <nhut@hotmail.fr>
2017-05-30 21:12:40 +0200

    Alert accueil, position fixed

--- public/elt/alert.php
+++ public/elt/alert.php
@@ -1,4 +1,4 @@
-<?php if(isset($alert)) { ?>
+<?php if(isset($alert) && $p != $page['accueil']) { ?>
     <div class="row">
         <div class="col-md-3"></div>
         <div class="col-md-6">
@@ -8,6 +8,15 @@
                 </button>
                 <span class="glyphicon glyphicon-alert position-icon"></span> <?php echo $alert; ?>
             </div>
+        </div>
+    </div>
+<?php } else if(isset($alert) && $p == $page['accueil']) { ?>   
+    <div class="alert-fix">
+        <div class="alert alert-danger center .alert-dismissible" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            <span class="glyphicon glyphicon-alert position-icon"></span> <?php echo $alert; ?>
         </div>
     </div>
 <?php } ?>
