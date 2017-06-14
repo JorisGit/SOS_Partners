a5824fc4ca2693142b4904118d8b6a3ac3723f9f
Joris Seyadou <joris@behind-shop.fr>
2017-06-14 14:42:19 +0100

    Ajout de la classe Sports

---
+++ models/Sports.php
@@ -0,0 +1,61 @@
+<?php
+
+class Sports{
+    
+    private $_id;
+    private $_intitule;
+    private $_type;
+
+    public function __construct(array $data) {
+        $this->hydrate($data);
+    }
+
+    public function hydrate(array $data) {
+        foreach($data as $key => $value) {
+            $method = 'set'.ucfirst($key);
+            if(method_exists($this, $method)) {
+                $this->$method($value);
+            }
+        }
+    }  
+    
+    public function getId(){
+        return $this->_id;
+    }
+    
+    public function getIntitule(){
+        return $this->_intitule;
+    }
+
+    public function getType(){
+        return $this->_type;
+    }
+    
+    public function setId($id) {
+        $this->_id = $id;
+    }
+    
+    public function setIntitule($intitule) {
+        $this->_intitule = $intitule;
+    }
+    
+    public function setType($type) {
+        $this->_type = $type;
+    }
+    
+}
+
+class Collectif extends Sports{
+
+    private $_nbJoueurs;
+    
+    public function getNbJoueurs(){
+        return $this->_nbJoueurs;
+    }
+    
+    public function setNbJoueurs($nbJoueurs) {
+        $this->_nbJoueurs = $nbJoueurs;
+    }
+}
+
+?>
