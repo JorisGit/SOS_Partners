56f03fecba4136f086f080e28f460dae0666bc34
nhut94 <nhut@hotmail.fr>
2017-06-15 19:14:31 +0200

    Link CSS

---
+++ public/elt/link.php
@@ -0,0 +1,8 @@
+    <link href="<?= $path['bootstrap']; ?>css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?= $path['font-awesome']; ?>/css/font-awesome.min.css" rel="stylesheet">
+    <?php if($p == $page['annonces']): ?>
+    <link href="//cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.css" rel="stylesheet" type="text/css" />
+    <?php endif; ?>
+    <link href="<?= $path['css']; ?>style.css" rel="stylesheet">
+    <link href="<?= $path['css']; ?>bootstrap_style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato|Lobster" rel="stylesheet">
