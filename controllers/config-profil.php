11c917287e0ec36e9863989817ca33b247507b2b
nhut94 <nhut@hotmail.fr>
2017-06-15 23:28:01 +0200

    Correction config profil controllers

--- controllers/config-profil.php
+++ controllers/config-profil.php
@@ -2,32 +2,38 @@
 
 if(isset($_POST['confirm'])) {
 
+    if(isset($_POST['supprimer-compte'])) {
+        $configProfil['supprimer-compte'] = htmlspecialchars($_POST['supprimer-compte']);
+    }
+
     $configProfil = array(
-        'newmdp' => htmlspecialchars(hash(hash_algos()[7], $_POST['newmdp'])),
-        'newmdp-confirm' => htmlspecialchars(hash(hash_algos()[7], $_POST['newmdp-confirm'])),
+        'password' => htmlspecialchars(hash(hash_algos()[7], $_POST['newmdp'])),
+        'mdp-confirm' => htmlspecialchars(hash(hash_algos()[7], $_POST['newmdp-confirm'])),
         'email' => htmlspecialchars($_POST['email']),
         'newemail' => htmlspecialchars($_POST['newemail']),
-        'supprimer-compte' => htmlspecialchars($_POST['supprimer-compte']),
         'mdp-confirm' => htmlspecialchars(hash(hash_algos()[7], $_POST['mdp'])),
     );
     
-    if($configProfil['mdp-confirm'] == $myProfil->getMdp()){
+    $modifProfil = new Profil($configProfil);
+
+    if($configProfil['mdp-confirm'] == $myProfil->getMdp()) {
         if(!empty($configProfil['newmdp']) AND !empty($configProfil['newmdp-confirm'])){
             if(preg_match('#(([a-zA-Z]+[0-9]+|[0-9]+[a-zA-Z]+).*|.*([a-zA-Z]+[0-9]+|[0-9]+[a-zA-Z]+).*|[a-zA-Z]+.*[0-9]+|[0-9]+.*[a-zA-Z])#', $configProfil['newmdp']) && strlen($configProfil['newmdp']) >= 6) {
-                if($configProfil['newmdp'] == $configProfil['newmdp-confirm']){
-                    $donnee = new Profil($configProfil);
-                    $update = new ProfilManager(getDb());
-                    $update->update($configProfil['newmdp']);
+                if($configProfil['newmdp'] == $configProfil['newmdp-confirm']) {
+                    if(!isset($_POST['supprimer-compte'])) {
+                        $profilManager->update($modifProfil);
+                    } else {
+                        $profilManager->delete($myProfil);
+                    }
                 } else {
                     $alert = "Votre nouveau mot de passe ne correspond pas à la confirmation de votre nouveau mot de passe.";
                 }
-            
             } else {
                 $alert = "Votre nouveau mot de passe n'est pas conforme, il faut minimum 1 majuscule, 1 chiffre, 1 caractère spécial et minimum 6 caractères.";
-            }
-            
-        } // Suite avec le mail à faire
-
+            }  
+        }
+    } else {
+        $alert = "Validation du mot de passe actuel incorrect";
     }
     
 
