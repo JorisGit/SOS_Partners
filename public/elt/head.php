2e0198c14119b553066b3573e8dc285db656a1c0
nhut94 <nhut@hotmail.fr>
2017-06-15 19:14:12 +0200

    include head

--- public/elt/head.php
+++ public/elt/head.php
@@ -2,9 +2,7 @@
     <title><?= $title; ?></title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="<?= $path['bootstrap']; ?>css/bootstrap.min.css" rel="stylesheet">
-    <link href="<?= $path['font-awesome']; ?>/css/font-awesome.min.css" rel="stylesheet">
-    <link href="<?= $path['css']; ?>style.css" rel="stylesheet">
-    <link href="<?= $path['css']; ?>bootstrap_style.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Lato|Lobster" rel="stylesheet">
+    <?php
+        include $path['elt'].'link.php';
+    ?>
 </head>
