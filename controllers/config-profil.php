538e4c4a139356b4a7fdd9662989165747b94315
nhut94 <nhut@hotmail.fr>
2017-06-15 10:17:19 +0200

    Ajout du isset

--- controllers/config-profil.php
+++ controllers/config-profil.php
@@ -1,13 +1,16 @@
 <?php
 
-$tableau = array(
+if(isset($_POST['confirm'])) {
 
-    'mdp' => htmlspecialchars(hash(hash_algos()[7], $_POST['mdp'])),
-    'newmdp' => htmlspecialchars(hash(hash_algos()[7], $_POST['newmdp'])),
-    'email' => htmlspecialchars($_POST['email']),
-    'newemail' => htmlspecialchars($_POST['newemail']),
-    'supprimer-compte' => htmlspecialchars($_POST['supprimer-compte']),
-    'mdp-confirm' => htmlspecialchars(hash(hash_algos()[7], $_POST['mdp'])),
-);
+    $tableau = array(
+        'mdp' => htmlspecialchars(hash(hash_algos()[7], $_POST['mdp'])),
+        'newmdp' => htmlspecialchars(hash(hash_algos()[7], $_POST['newmdp'])),
+        'email' => htmlspecialchars($_POST['email']),
+        'newemail' => htmlspecialchars($_POST['newemail']),
+        'supprimer-compte' => htmlspecialchars($_POST['supprimer-compte']),
+        'mdp-confirm' => htmlspecialchars(hash(hash_algos()[7], $_POST['mdp'])),
+    );
+    
+}
 
 ?>
