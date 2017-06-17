b8013375cd55f0d585187f6db215592ea211639f
JorisGIT <joris@behind-shop.fr>
2017-06-17 14:29:20 +0200

    fixes #2, Ajout de conditions si le mot de passe est vide

--- controllers/config-profil.php
+++ controllers/config-profil.php
@@ -1,45 +1,43 @@
 <?php
-
 if(isset($_POST['confirm'])) {
 
     if(isset($_POST['supprimer-compte'])) {
         $configProfil['supprimer-compte'] = htmlspecialchars($_POST['supprimer-compte']);
     }
-
     $configProfil = array(
-        'password' => htmlspecialchars(hash(hash_algos()[7], $_POST['newmdp'])),
-        'newmdp-confirm' => htmlspecialchars(hash(hash_algos()[7], $_POST['newmdp-confirm'])),
         'email' => htmlspecialchars($_POST['email']),
         'newemail' => htmlspecialchars($_POST['newemail']),
         'mdp-confirm' => htmlspecialchars(hash(hash_algos()[7], $_POST['mdp'])),
         'pseudo' => htmlspecialchars($myProfil->getPseudo())
     );
-
-
+    if(!empty($_POST['nouveaumdp'])) {
+        $configProfil['password'] = htmlspecialchars(hash(hash_algos()[7], $_POST['nouveaumdp']));
+    }
+    if(!empty($_POST['nouveaumdpconfirm'])) {
+        $configProfil['nouveaumdpconfirm'] = htmlspecialchars(hash(hash_algos()[7], $_POST['nouveaumdpconfirm']));
+    }
     $modifProfil = new Profil($configProfil);
     $ProfilManager = new ProfilManager(getDb());
+    var_dump($configProfil);
     if($configProfil['mdp-confirm'] == $myProfil->getMdp()) {
-        if(!empty($configProfil['password']) AND !empty($configProfil['newmdp-confirm'])){
-            if($configProfil['password'] == $configProfil['newmdp-confirm']) {
-              $ProfilManager->updateMdp($modifProfil);
-              $alert = "Votre mot de passe a bien été changé.";
+        if(!empty($configProfil['password']) AND !empty($configProfil['nouveaumdpconfirm'])){
+            if($configProfil['password'] == $configProfil['nouveaumdpconfirm']) {
+               $ProfilManager->updateMdp($modifProfil);
+                $alert = "Votre mot de passe a bien été changé.";
             } else {
                 $alert = "Votre nouveau mot de passe ne correspond pas à la confirmation de votre nouveau mot de passe.";
             }
         }
         if(!empty($configProfil['email']) AND !empty($configProfil['newemail'])){
             if($configProfil['email'] == $configProfil['newemail']){
-              $ProfilManager->updateEmail($modifProfil);
-              $alert = "Votre adresse email a bien été changé.";
+                $ProfilManager->updateEmail($modifProfil);
+                $alert = "Votre adresse email a bien été changé.";
             } else {
-              $alert = "Votre nouvelle adresse email ne correspond pas à la validation de votre nouvelle adresse email.";
+                $alert = "Votre nouvelle adresse email ne correspond pas à la validation de votre nouvelle adresse email.";
             }
         }
     } else {
         $alert = "Validation du mot de passe actuel incorrect";
     }
-
-
 }
-
 ?>
--- views/config-profil.php
+++ views/config-profil.php
@@ -13,13 +13,13 @@
                 <div class="col-md-6">
                     <div class="form-group">
                         <label class="control-label" for="mdp-label">Nouveau mot de passe</label>
-                        <input id="newmdp" name="newmdp" type="password" placeholder="Votre nouveau mot de passe" class="form-control input-md" >
+                        <input id="nouveaumdp" name="nouveaumdp" type="password" placeholder="Votre nouveau mot de passe" class="form-control" >
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="form-group">
                         <label class="control-label" for="newmdp-label">Confirmation du nouveau mot de passe</label>
-                        <input id="newmdp-confirm" name="newmdp-confirm" type="password" placeholder="Votre nouveau mot de passe" class="form-control input-md" >
+                        <input id="nouveaumdpconfirm" name="nouveaumdpconfirm" type="password" placeholder="Votre nouveau mot de passe" class="form-control input-md" >
                     </div>
                 </div>
             </div>
@@ -51,15 +51,33 @@
                 <div class="col-md-6">
                     <div class="form-grp check-grp">
                         <div class="[ form-group ]">
-                            <input type="checkbox" name="supprimer-compte" id="supprimer-compte" autocomplete="off"/>
+                            <input type="checkbox" name="supprimer-compte" id="supprimer-compte" autocomplete="off" data-toggle="modal" data-target="#myModal"/>
                             <div class="[ btn-group ]">
                                 <label for="supprimer-compte" class="[ btn btn-primary ]">
                                     <span class="[ glyphicon glyphicon-ok ]"></span>
                                     <span> </span>
                                 </label>
-                                <label for="supprimer-compte" class="[ btn btn-default active ]">
+                                <label for="supprimer-compte" class="[ btn btn-default active ]" >
                                     Je supprime mon compte définitivement
                                 </label>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Modal -->
+                    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                    <h4 class="modal-title" id="myModalLabel">Suppression de compte</h4>
+                                </div>
+                                <div class="modal-body">
+                                    <p>En cochant cette case et en cliquant sur le bouton "Confirmer", votre compte sera supprimé.</p>
+                                    <p>Il sera possible de le récupérer dans les 15 jours qui suivent après la suppression de votre compte en contactant le support.</p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-primary" data-dismiss="modal">Je confirme</button>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -76,7 +94,7 @@
                         <label class="control-label" for="mdp-label">Mot de passe actuel</label>
                         <input id="mdp-confirm" name="mdp" type="password" placeholder="Votre mot de passe actuel" class="form-control input-md" required>
                         <span class="help-block"><i class="fa fa-lightbulb-o" aria-hidden="true"></i>
-Vous devez entrer votre mot de passe pour pouvoir effectuer les changements demandés</span>
+                        Vous devez entrer votre mot de passe pour pouvoir effectuer les changements demandés</span>
                     </div>
                 </div>
             </div>
