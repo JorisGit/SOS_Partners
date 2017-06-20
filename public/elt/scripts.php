5b9b6154ec3a4925edaa56a72312ca9cafc24fec
nhut94 <nhut@hotmail.fr>
2017-06-20 21:35:15 +0200

    Google API en commentaire

--- public/elt/scripts.php
+++ public/elt/scripts.php
@@ -1,7 +1,7 @@
 <script type="text/javascript" src="<?= $path['js']; ?>jquery.js"></script>       
 <script type="text/javascript" src="<?= $path['bootstrap']; ?>js/bootstrap.min.js"></script>
-<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDdIXaZYZKPRta7Sych_gAkMksTgM6Z0PU&callback=initMap"
-  type="text/javascript"></script>
+<!--<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDdIXaZYZKPRta7Sych_gAkMksTgM6Z0PU&callback=initMap"
+  type="text/javascript"></script>-->
 <?php if($p == $page['annonces']): ?>
 <script type="text/javascript" src="//cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
 <script type="text/javascript" src="//cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.js"></script>
