d0ef932f7d4e76a16f61a862fde792500b04dee3
nhut94 <nhut@hotmail.fr>
2017-06-20 21:36:10 +0200

    Ajout fonction strAttr qui permets de transformer un string afin qu'il soit compatible à un attribut html

--- controllers/global.php
+++ controllers/global.php
@@ -1,4 +1,15 @@
 <?php
+    function strAttr($str) {
+
+    $str = htmlentities($str, ENT_NOQUOTES, 'utf-8');
+    $str = preg_replace('#&([A-za-z])(?:acute|cedil|caron|circ|grave|orn|ring|slash|th|tilde|uml);#', '\1', $str);
+    $str = preg_replace('#&([A-za-z]{2})(?:lig);#', '\1', $str); // pour les ligatures e.g. '&oelig;'
+    $str = preg_replace('#&[^;]+;#', '', $str); // supprime les autres caractères
+    $str = strtolower(str_replace(' ', '-', $str));
+    
+    return $str;
+    }
+
     function chargerClasse($classe) {
         require 'models/'.$classe.'.php';
     }
