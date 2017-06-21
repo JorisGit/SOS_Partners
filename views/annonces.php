44a3569db74fcc94e31a06684c60869e9fad4d5c
nhut94 <nhut@hotmail.fr>
2017-06-21 02:05:04 +0200

    Vicopo location

--- views/annonces.php
+++ views/annonces.php
@@ -62,29 +62,18 @@
                 </div>
                 <div class="col-md-5">
                     <div class="form-grp">
-                        <label for="ville"><i class="fa fa-map-marker" aria-hidden="true"></i>Ville</label>
-                        <input id="ville" type="text" name="ville" class="form-control">
+                        <label for="city"><i class="fa fa-map-marker" aria-hidden="true"></i>Ville</label>
+                        <input id="city" type="text" name="city" class="form-control">
                         <span class="help-block"><i class="fa fa-lightbulb-o" aria-hidden="true"></i>Paramètre non pris en compte si le champ est vide</span>
+                        <div id="output"></div>
                     </div>
                 </div>
                 <div class="col-md-2"></div>
                 <div class="col-md-5">
                     <div class="form-grp">
-                        <label for="codepostal"><i class="fa fa-map-marker" aria-hidden="true"></i>Code Postal</label>
-                        <input id="codepostal" type="text" name="codepostal" class="form-control">
+                        <label for="code"><i class="fa fa-map-marker" aria-hidden="true"></i>Code Postal</label>
+                        <input id="code" type="text" name="code" class="form-control">
                         <span class="help-block"><i class="fa fa-lightbulb-o" aria-hidden="true"></i>Paramètre non pris en compte si le champ est vide</span>
-                        <p>
-                            <label>
-                                Tapez le début d'un nom de ville ou d'un code postal puis appuyez sur <b>Entrée</b>.<br>
-                                <input id="ville" placeholder="Entrez un code postal ou une ville" autocomplete="off" size="50">
-                            </label>
-                        </p>
-                        <ul>
-                            <li data-vicopo="#ville">
-                                <strong data-vicopo-code-postal></strong>
-                                <span data-vicopo-ville></span>
-                            </li>
-                        </ul>
                     </div>
                 </div>
                 <div class="col-md-5"></div>
